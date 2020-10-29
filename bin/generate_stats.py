981d81e22836741438243100c6743319b5aba0bd
Vincent Viers <vincent.viers@beta.gouv.fr>
2020-10-29 15:43:54 +0100

    (feat) basic CLI + clean lib.cgus

---
+++ bin/generate_stats.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+import json
+import logging
+from pathlib import Path
+from datetime import datetime
+
+import click
+
+from lib.cgus import CGUsDataset, CGU
+
+logging.basicConfig(level=logging.INFO)
+
+@click.command()
+@click.option('-p', '--path', default="../CGUs-versions", help="Path to your local CGUs-versions repository.")
+@click.option('-o', '--outdir', default="./reports", help="Output folder for the generated statistics.")
+def cli(path, outdir):
+    return main(path, outdir)
+
+def main(path, outdir):
+    logging.info(f"Generating stats for data in {path}")
+    dataset = CGUsDataset(path)
+
+    all_stats = dict()
+
+    for file_path in dataset.yield_all_md(ignore_rootdir=True):
+        cgu = CGU(file_path)
+        all_stats[cgu.fullname] = cgu.to_dict()
+
+    _save_to_file(all_stats, outdir, f"stats_{datetime.now().strftime('%Y%m%d_%H%M%S')}.json")
+
+    logging.info(f"Saved stats in {outdir}/stats.json")
+
+def _save_to_file(data: dict, outdir, filename):
+    out_path = Path(outdir)
+    # create destination folder if it does not exist
+    out_path.mkdir(parents=True, exist_ok=True)
+    # save data
+    full_path = out_path / filename
+    full_path.write_text(json.dumps(data))
+
+if __name__ == "__main__":
+    cli()
--- lib/cgus.py
+++ lib/cgus.py
@@ -1,7 +1,11 @@
 import json
+import logging
 from pathlib import Path, PosixPath
 
-from nltk import word_tokenize
+import nltk
+
+logging.basicConfig(level=logging.INFO)
+
 
 class CGUsDataset():
     """
@@ -14,7 +18,7 @@ class CGUsDataset():
         assert self.root_path.is_dir(), f"{root_path} is not a directory"
 
 
-    def _yield_all_md(self, ignore_rootdir: bool = True) -> list:
+    def yield_all_md(self, ignore_rootdir: bool = True) -> list:
         if ignore_rootdir:
             return self.root_path.glob('**/*/*.md')
         else:
@@ -25,38 +29,34 @@ class CGU():
     A CGU object, and the main building bloc for our algorithms.
     """
 
+    # Remove all punctuation when tokenizing text
+    _TOKENIZER = nltk.tokenize.RegexpTokenizer(r'\w+')
+
     def __init__(self, path: PosixPath):
         self._path = path
         self.name = self._path.name
         self.source = self._path.as_posix().split("/")[-2]
-        self.fullname = f"{self.source} - {self.name}"
-        self.cgu_type = "TODO" #TODO add CGU type
+        self.fullname = f"{self.source} - {self.name}".removesuffix(".md")
+        self.cgu_type = f"{self.name}".removesuffix(".md") #TODO check that this is the way to go about it
         self.raw_content = self._path.read_text()
-        self.parsed_content = word_tokenize(self.raw_content)
+        self.tokens = self._TOKENIZER.tokenize(self.raw_content)
 
 
-    def to_dict(self, json: bool = False):
+    def to_dict(self) -> dict:
         """
-            Serialize the CGU object to key/value pairs.
-            Returns a dict object by default.
-            If `json`, returns a json string.
+            "Serialize" the CGU object to key/value pairs.
         """
         return {
-            "full_name": self.fullname,
             "cgu_type": self.cgu_type,
-            "raw_content": self.raw_content
+            "num_words": len(self)
         }
+
+    def most_used_words(self, n: int = 10):
+        return nltk.FreqDist(self.tokens).most_common(n)
 
     def __str__(self):
         return f"\n{self.fullname}\nLength: {len(self)}\n{self.raw_content[:500]}"
 
     def __len__(self):
-        """Length of a CGU is its number of words."""
-        return len(self.parsed_content)
-
-
-if __name__ == "__main__":
-    p = Path("../CGUs-versions/Google Ads/Trackers Policy.md")
-    cgu = CGU(p)
-    print(cgu.to_dict())
-    print(len(cgu))
+        """Length of a CGU is its number of words/tokens."""
+        return len(self.tokens)
